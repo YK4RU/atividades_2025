6bdb99a6a7b25afee8989eaa231e5a255449f5f1
YK4RU <168197162+YK4RU@users.noreply.github.com>
2025-04-08 16:53:58 -0300

    Add files via upload

---
+++ atv/q1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    $nomeV1 = '56';
+    $nomeV2 = '65';
+    echo $nomeV1;
+    echo '<br>';
+    echo $nomeV2;
+?>
+
+</body>
+</html>
